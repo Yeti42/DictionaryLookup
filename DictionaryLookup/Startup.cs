1ad9d4a8e49589f8b453c0bd2d74de3152c79198
Dave <davestev@microsoft.com>
2016-07-13 17:08:34 -0700

    Add project files.

---
+++ DictionaryLookup/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DictionaryLookup.Startup))]
+namespace DictionaryLookup
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
