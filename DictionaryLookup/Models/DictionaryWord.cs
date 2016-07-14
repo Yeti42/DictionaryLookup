8aa6715e31f08863602d3a529f1225cb3f47fe94
Dave <davestev@microsoft.com>
2016-07-13 17:37:49 -0700

    Initial dictionary lookup web app

---
+++ DictionaryLookup/Models/DictionaryWord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Globalization;
+
+namespace DictionaryLookup.Models
+{
+    public class DictionaryWord
+    {
+        public int DictionaryWordID { get; set; }
+        public string Word { get; set; }
+        public bool Restricted { get; set; }
+        public double UnigramProbability { get; set; }
+        public string Self
+        {
+            get
+            {
+                return string.Format(CultureInfo.CurrentCulture,
+               "api/word/{0}", this.Word);
+            }
+            set { }
+        }
+    }
+}
---
+++ DictionaryLookup/Models/LanguageDictionary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Globalization;
+
+namespace DictionaryLookup.Models
+{
+    public class LanguageDictionary
+    {
+        public int LanguageDictionaryID { get; set; }
+        public string BCP47 { get; set; }
+        public string Self
+        {
+            get
+            {
+                return string.Format(CultureInfo.CurrentCulture,
+               "api/dictionary/{0}", this.BCP47);
+            }
+            set { }
+        }
+    }
+}
